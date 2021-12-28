bd81ae5b971ad8f64107ebddc6d9ad1531f1cb5b
mcm666666 <41454252+mcm666666@users.noreply.github.com>
2021-12-28 08:51:48 -0800

    Update Regular Expressions I.js

--- Day 7/Regular Expressions I.js
+++ Day 7/Regular Expressions I.js
@@ -1,1 +1,8 @@
+//Complete the function in the editor below by returning a RegExp object, , that matches any string  that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.
 
+function regexVar() {
+  var re = RegExp(/^([aeiou].*\1$/);
+  
+  
+  return re;
+}
