18370ddc85cf443b8b22bb8fb43701c5e2b7db87
mcm666666 <41454252+mcm666666@users.noreply.github.com>
2021-12-28 09:04:07 -0800

    Update Regular Expressions II.js

--- Day 7/Regular Expressions II.js
+++ Day 7/Regular Expressions II.js
@@ -1,1 +1,11 @@
+/* Complete the function in the editor below by returning a RegExp object, 
+re, that matches any string  satisfying both of the following conditions:
+  * String  starts with the prefix Mr., Mrs., Ms., Dr., or Er.
+  * The remainder of string  (i.e., the rest of the string after the prefix) 
+    consists of one or more upper and/or lowercase English alphabetic letters (i.e., [a-z] and [A-Z]). */
 
+function regexVar() {
+  var re = (/^(Mr|Mrs|Ms|Dr|er)(\.)([a-zA-Z])+$/);
+  
+  return re;
+}
