c7f2684ffbb1c53e05460283efe843fd44b7c479
mcm666666 <41454252+mcm666666@users.noreply.github.com>
2021-12-28 09:13:27 -0800

    Update Regular Expressions III.js

--- Day 7/Regular Expressions III.js
+++ Day 7/Regular Expressions III.js
@@ -1,1 +1,8 @@
+/* Complete the function in the editor below by returning a RegExp object, re, 
+  that matches every integer in some string . */
 
+function regexVar() {
+ let re = /\d+/g;
+  
+  return re;
+}
